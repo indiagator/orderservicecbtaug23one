6209fada41e3a8b1a76a3275e2591684228df3b2
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-04 10:32:09 +0530

    webclient added

--- src/main/java/com/cbt/orderservicecbtaug23one/MainRestController.java
+++ src/main/java/com/cbt/orderservicecbtaug23one/MainRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/v1")
@@ -22,6 +24,9 @@ public class MainRestController
     @Autowired
     PaymentRepository paymentRepository;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @PostMapping("save/order")
     public ResponseEntity<Order> saveOrder(@RequestBody Order order)
     {
@@ -36,19 +41,26 @@ public class MainRestController
     }
 
     @PostMapping("save/order/status")
-    public ResponseEntity<Orderstatus> saveOrderStatus(@RequestBody Orderstatus orderstatus)
+    public ResponseEntity<Mono<Payment>> saveOrderStatus(@RequestBody Orderstatus orderstatus)
     {
         orderstatus.setId(String.valueOf((int)(Math.random()*100000)));
-        orderstatusRepository.save(orderstatus);
+        orderstatusRepository.save(orderstatus); // THIS SHOULD BE AN UPDATE OPERATION
 
-        Payment payment = new Payment();
-        payment.setId(String.valueOf((int)(Math.random()*100000)));
-        payment.setOrderid(orderstatus.getOrderid());
-        payment.setOfferid(orderRepository.findById(orderstatus.getOrderid()).get().getOfferid());
-        payment.setStatus("DUE");
+        Mono<Payment> response = null;
+        if(orderstatus.getStatus().equals("ACCEPTED"))
+        {
+            // send a request to create payment to payment service
+            response =  webClientBuilder.build().post().uri("http://localhost:8072/payment-service/api/v1/save/payment").
+                    body(Mono.just(orderstatus),Orderstatus.class).retrieve().bodyToMono(Payment.class);
+        }
 
-        paymentRepository.save(payment);
+        //Payment payment = new Payment();
+        //payment.setId(String.valueOf((int)(Math.random()*100000)));
+        //payment.setOrderid(orderstatus.getOrderid());
+        //payment.setOfferid(orderRepository.findById(orderstatus.getOrderid()).get().getOfferid());
+        //payment.setStatus("DUE");
+        //paymentRepository.save(payment);
 
-        return new ResponseEntity<>(orderstatus,HttpStatus.OK);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 }
